604ff67d2973e972ff20cf2b8fde9a2f57d91486
Sakhi Akram <sakhiakram.github@outlook.com>
2025-11-15 11:06:10 +0500

    exercise 1.9 solved

--- unicafe/src/components/Statistics.jsx
+++ unicafe/src/components/Statistics.jsx
@@ -1,19 +1,26 @@
 import React from 'react'
 
-const Statistics = ({good, neutral, bad}) => {
+const Statistics = ({ good, neutral, bad }) => {
     const total = good + neutral + bad;
     const average = total ? (good - bad) / total : 0
     const positive = total ? ((good / total) * 100).toFixed(1) : '0.0'
     return (
-        <>
-            <h1>statistics</h1>
-            <p>good {good}</p>
-            <p>neutral {neutral}</p>
-            <p>bad {bad}</p>
-            <p>all {total}</p>
-            <p>average {average}</p>
-            <p>positive {positive} %</p>
-        </>
+        total === 0 ? (
+            <>
+             <h1>statistics</h1>
+            <p>No feedback given</p>
+            </>
+        ) : (
+            <>
+                <h1>statistics</h1>
+                <p>good {good}</p>
+                <p>neutral {neutral}</p>
+                <p>bad {bad}</p>
+                <p>all {total}</p>
+                <p>average {average}</p>
+                <p>positive {positive} %</p>
+            </>
+        )
     )
 }
 
